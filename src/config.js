e81befda42a8cc94c78be3a7932a7973417d4695
Alex Walters <alexnwalters@gmail.com>
2019-10-23 00:29:53 +0000

    update deploy

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export default {
     BREWERY_ENDPOINT: `https://sandbox-api.brewerydb.com/v2`,
     BREWERY_KEY: process.env.REACT_APP_BREWERY_DB_API_KEY,
-    API_ENDPOINT: 'https://evening-scrubland-27594-beer.herokuapp.com/api',  // Production Endpoint API_ENDPOINT: 'http://localhost:8000/api' for local
+    API_ENDPOINT: process.env.REACT_APP_API_ENDPOINT || 'http://localhost:8000/api',
     TOKEN_KEY: 'Beer-Diary-Client-Auth-Token',
 }
